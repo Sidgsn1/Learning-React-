9ce416517044d8ecbfa65152841b9d01d5ba3b48
Sidgsn1 <sidgsn101@gmail.com>
2026-06-25 07:33:18 +0530

    ch3

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const heading=React.createElement(
-    "h1",
-    {id:"heading"},
-    "hello from react"
-)
+const Heading1=()=><h1>hello from jsx</h1>
+
+const Heading=()=>{
+    return (
+        <div>
+            <Heading1 />
+            <h1>Hello from functional component</h1>
+        </div>
+    )
+}
 
 const root=ReactDOM.createRoot(document.querySelector("#root"))
-root.render(heading)
+root.render(<Heading />)
